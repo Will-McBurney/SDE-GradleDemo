1a788378bf788d06092ddcb85e93df5e970626f7
Will-McBurney <pmcburn1@gmail.com>
2022-06-22 23:12:27 -0400

    Added tests to the build functionality

--- src/test/java/edu/virginia/cs/FunctionsTest.java
+++ src/test/java/edu/virginia/cs/FunctionsTest.java
@@ -1,10 +1,9 @@
 package edu.virginia.cs;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-
-import edu.virginia.cs.Functions;
 
 public class FunctionsTest {
 
@@ -12,6 +11,18 @@ public class FunctionsTest {
 	void test() {
 		String[] args = {"1", "9"};
 		Functions.main(args);
+	}
+	
+	@Test
+	void testSum() {
+		double r = Functions.sum(1, 9);
+		assertEquals(10.0, r, 1e-10);
+	}
+	
+	@Test
+	void testProduct() {
+		double r = Functions.product(1, 9);
+		assertEquals(9.0, r, 1e-10);
 	}
 
 }
