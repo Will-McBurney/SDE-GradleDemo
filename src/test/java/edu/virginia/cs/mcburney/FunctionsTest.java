f658087e4172e01a0112fdfb4b03390ebc9f6a20
Will-McBurney <pmcburn1@gmail.com>
2022-06-22 21:43:38 -0400

    Added tests, updated build settings

---
+++ src/test/java/edu/virginia/cs/mcburney/FunctionsTest.java
@@ -0,0 +1,15 @@
+package edu.virginia.cs.mcburney;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class FunctionsTest {
+
+	@Test
+	void test() {
+		String[] args = {"1", "9"};
+		Functions.main(args);
+	}
+
+}
